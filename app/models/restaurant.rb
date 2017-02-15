250bb9b6154b8229365da7a4becb457dfada7b5a
Turo Hakulinen <turo.hakulinen@gmail.com>
2017-02-15 17:13:41 +0000

    + average rating

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,12 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }, presence: true
-
+  def avg_rating
+    return nil if reviews.length == 0
+    count = 0.0
+    reviews.each do |r|
+      count += r.rating
+    end
+    (count / reviews.length).round(2)
+  end
 end
